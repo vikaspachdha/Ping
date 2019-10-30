241207fb9e7b2c9a49d672c946fcfe45358d0e98
Vikas Pachdha <vikas.pachdha@qt.io>
2019-10-30 15:21:43 +0100

    Play alarm ringtone

--- app/src/main/java/com/vikaspachdha/ping/MainActivity.kt
+++ app/src/main/java/com/vikaspachdha/ping/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.vikaspachdha.ping
 
+import android.media.RingtoneManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+        val r =  RingtoneManager.getRingtone(this, uri)
+        monitorSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked && !r.isPlaying) {
+                r.play();
+            } else if (!isChecked && r.isPlaying) {
+                r.stop();
+            }
+        }
     }
 }
