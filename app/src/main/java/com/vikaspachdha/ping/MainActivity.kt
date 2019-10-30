f1018a93384fbffa61dad7bf48133e4051c05cef
Vikas Pachdha <vikas.pachdha@qt.io>
2019-10-30 21:57:18 +0100

    Add alarm file
    Change-Id: I8c3277ae17065c687f901974531426d662355c8c

--- app/src/main/java/com/vikaspachdha/ping/MainActivity.kt
+++ app/src/main/java/com/vikaspachdha/ping/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.vikaspachdha.ping
 
-import android.media.RingtoneManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
 
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-        val r =  RingtoneManager.getRingtone(this, uri)
+
+        var mPlayer = MediaPlayer.create(this, R.raw.alarm)
+        //mPlayer.setVolume(1.toFloat(), 1.toFloat())
+        //mPlayer.prepare()
         monitorSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
-            if (isChecked && !r.isPlaying) {
-                r.play();
-            } else if (!isChecked && r.isPlaying) {
-                r.stop();
+            if (isChecked) {
+                mPlayer.start()
+            } else if (!isChecked) {
+                mPlayer.stop()
+                mPlayer = MediaPlayer.create(this, R.raw.alarm)
             }
         }
     }
