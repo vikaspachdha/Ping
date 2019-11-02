d4b4c5cb49824af05f2f920c543eb2d099a1956c
Vikas Pachdha <vikas.pachdha@qt.io>
2019-11-02 23:49:54 +0100

    Ping the IP address

--- app/src/main/java/com/vikaspachdha/ping/PingService.kt
+++ app/src/main/java/com/vikaspachdha/ping/PingService.kt
@@ -4,22 +4,44 @@ import android.app.Notification
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import java.net.Inet4Address
 
 class PingService : Service() {
+
+    enum class State {
+        IDLE,
+        CONNECTED,
+        DISCONNECTED
+    }
+
+    private var m_state = State.IDLE
     private var m_ip: String = ""
     var stopPing = false
-    var pingThread = Thread(
-        Runnable {
-            var count = 0
-            while (!this.stopPing) {
-                Thread.sleep(1000)
-                println("Pinging ${++count}")
-            }
+    var pingThread = Thread{doPing()}
+
+
+    private fun doPing() {
+        val ip = Inet4Address.getByName(m_ip)
+        while (!stopPing) {
+            setState(when(ip.isReachable(500)){
+                true -> State.CONNECTED
+                false -> State.DISCONNECTED
+            })
+            Thread.sleep(1000)
         }
-    )
+        setState(State.IDLE)
+    }
+
+    private fun setState(newState: State) {
+        if (newState != this.m_state) {
+            synchronized(this) {
+                this.m_state = newState
+            }
+            println("State changed $m_state")
+        }
+    }
 
     private fun buildNotification():Notification {
-        val notificationID = 101
         var b = Notification.Builder(applicationContext, getString(R.string.channel_id))
         b.setContentTitle("Pinging")
         b.setContentText("Pinging IP $m_ip")
