27e5aecfb1a5ce683a563adc5c7226cc7eb843ad
Vikas Pachdha <vikas.pachdha@qt.io>
2019-11-10 21:22:32 +0100

    Code cleanup

--- app/src/main/java/com/vikaspachdha/ping/PingService.kt
+++ app/src/main/java/com/vikaspachdha/ping/PingService.kt
@@ -1,6 +1,7 @@
 package com.vikaspachdha.ping
 
 import android.app.Notification
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
@@ -10,9 +11,9 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import android.content.BroadcastReceiver
 import android.content.IntentFilter
 import android.os.Vibrator
+import android.util.Log
 
-
-
+const val gLogTag = "PingService"
 
 class PingService : Service() {
 
@@ -27,52 +28,59 @@ class PingService : Service() {
         val actionPong = PingService::class.java.name + ".PONG"
     }
 
-
-    private var m_state = State.IDLE
-    private var m_ip: String = ""
-    private var stopPing = false
-    private var pingThread = Thread{doPing()}
+    private var mNotificationManager: NotificationManager? = null
+    private var mState = State.IDLE
+    private var mIp: String = ""
+    private var mStopPing = false
+    private var mPingThread = Thread { doPing() }
+    private var mMissedCount = 0
 
     private var mReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             if (intent.action == actionPing) {
-                println("Received ping")
-                LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(Intent(actionPong))
+                Log.d(gLogTag, "Received ping")
+                LocalBroadcastManager.getInstance(applicationContext)
+                    .sendBroadcast(Intent(actionPong))
             }
         }
     }
 
-
     private fun doPing() {
-        val ip = Inet4Address.getByName(m_ip)
-        var count = 0
-        while (!stopPing) {
-            println("Checking IP ${count++}")
-            setState(when(ip.isReachable(500)){
-                true -> State.CONNECTED
-                false -> State.DISCONNECTED
-            })
+        val ip = Inet4Address.getByName(mIp)
+        while (!mStopPing) {
+            if (ip.isReachable(500)) {
+                Log.d(gLogTag, "Connected")
+                setState(State.CONNECTED)
+                mMissedCount = 0
+            } else {
+                Log.d(gLogTag, "Ping failed: $mMissedCount")
+                if (++mMissedCount > resources.getInteger(R.integer.panic_threshold)) {
+                    Log.d(gLogTag, "Disconnected")
+                    setState(State.DISCONNECTED)
+                }
+            }
             Thread.sleep(1000)
         }
         setState(State.IDLE)
     }
 
     private fun setState(newState: State) {
-        if (newState != this.m_state) {
+        if (newState != this.mState) {
             synchronized(this) {
-                this.m_state = newState
+                this.mState = newState
             }
-            if (this.m_state == State.DISCONNECTED) {
-                goPanick()
+            Log.d(gLogTag, "State changed $mState")
+            if (this.mState == State.DISCONNECTED) {
+                goPanic()
             }
-            println("State changed $m_state")
+            updateNotification()
         }
     }
 
-    private fun buildNotification():Notification {
+    private fun buildNotification(): Notification {
         var b = Notification.Builder(applicationContext, getString(R.string.channel_id))
-        b.setContentTitle("Pinging")
-        b.setContentText("Pinging IP $m_ip")
+        b.setContentTitle("Pinging $mIp")
+        b.setContentText(notificationText())
         b.setSmallIcon(android.R.drawable.ic_dialog_alert)
         b.setChannelId(getString(R.string.channel_id))
         return b.build()
@@ -90,19 +98,19 @@ class PingService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val ip = intent?.getStringExtra(getString(R.string.data_ip_address))
-        m_ip = "Invalid IP"
+        mIp = "Invalid IP"
         if (ip != null) {
-            m_ip = ip
+            mIp = ip
         }
         val notification = buildNotification()
-        startForeground(541, notification)
-        this.pingThread.start()
+        startForeground(resources.getInteger(R.integer.notification_id), notification)
+        this.mPingThread.start()
         return START_STICKY
     }
 
     override fun onDestroy() {
-        this.stopPing = true
-        this.pingThread.join()
+        this.mStopPing = true
+        this.mPingThread.join()
         super.onDestroy()
     }
 
@@ -113,14 +121,31 @@ class PingService : Service() {
     }
 
     private fun soundAlarm() {
-        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        if (vibrator.hasVibrator()) {
-            vibrator.vibrate(500) // for 500 ms
-        }
+//        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+//        if (vibrator.hasVibrator()) {
+//            vibrator.vibrate(500) // for 500 ms
+//        }
     }
 
-    private fun goPanick() {
+    private fun goPanic() {
+        Log.i(gLogTag, "Going panic")
         showUI()
         soundAlarm()
+    }
+
+    private fun updateNotification() {
+        Log.d(gLogTag, "Updating notification")
+        val notification = buildNotification()
+        notification.flags = Notification.FLAG_ONGOING_EVENT
+        mNotificationManager?.notify(resources.getInteger(R.integer.notification_id), notification)
+    }
+
+    private fun notificationText(): String {
+        val statusStr = when (mState) {
+            State.CONNECTED -> "Connected"
+            State.DISCONNECTED -> "PANIC"
+            else -> "Idle"
+        }
+        return "Current state: $statusStr"
     }
 }
